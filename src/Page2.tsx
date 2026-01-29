8e13543f2be1a1dcc7ddd0314560aeb26b544085
Corentin JACOB <cjacob@ippon.fr>
2026-01-29 16:07:17 +0100

    feat: main page

---
+++ src/Page2.tsx
@@ -0,0 +1,12 @@
+import "./Page2.css";
+
+function Page2() {
+  return (
+    <div className="page2-container">
+      <h1>Deuxième Page</h1>
+      <p>Contenu de la deuxième page</p>
+    </div>
+  );
+}
+
+export default Page2;
